6a8ad731771365e6a6d54d42fd6c70a894decbf5
N3verl0sen <nverlusen@gmail.com>
2024-10-22 09:47:50 -0300

    A new C code

---
+++ text.c
@@ -0,0 +1,25 @@
+
+//Code One
+
+#include <stdio.h> 
+
+int main() 
+{
+    char car; 
+
+    printf("Digite um caractere e ENTER\n"); 
+
+    scanf("%c", &car); 
+
+    printf("Caractere digitado: %c\n", car); 
+
+    return 0; 
+}
+
+//Code Two
+
+int a = 2; 
+int b;
+
+b = a++; 
+b = +a;  
